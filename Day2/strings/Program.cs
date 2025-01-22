d82e338721cf47b275572265e36dc1a5c522e798
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-01-22 13:34:31 +0530

    string concatenation

---
+++ Day2/strings/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console.WriteLine("hello");
+// Console.WriteLine(@"C:\media\hello.txt");
+
+string fname = "Ishini";
+string lname = "chanrindi";
+
+string fullname =  fname +" " + lname +" "+ "dewamiththa";
+
+Console.WriteLine(fullname);
